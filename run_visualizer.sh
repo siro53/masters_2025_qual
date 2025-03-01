73d6569cb45888c121c6b215f6a9afe281420c71
siro53 <shusuke.shirotake@me.com>
2025-03-02 01:19:35 +0900

    シェルスクリプトの修正

--- run_visualizer.sh
+++ run_visualizer.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+VIS_DIR=$(pwd)/visualizer-template-public
+WASM_DIR=$VIS_DIR/wasm
+
 if [ -z $YARN_INIT ]; then
     yarn
 fi
 
-cd wasm && wasm-pack build --target web --out-dir ../public/wasm && cd ..
+cd $WASM_DIR
+wasm-pack build --target web --out-dir ../public/wasm
+cd $VIS_DIR
 yarn dev
